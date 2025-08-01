33c229af3eac469bb3872ba9203c30322f362f8d
Hizkia-wa <siahaanhizkia06@gmail.com>
2025-08-01 12:54:34 +0700

    pendaftaran dan logo

--- codeprogram/resources/views/siswa/pendaftaran_success.blade.php
+++ codeprogram/resources/views/siswa/pendaftaran_success.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-12">
             <h2>Pendaftaran Berhasil!</h2>
             <p>Terima kasih telah mendaftar. Data Anda telah berhasil disimpan.</p>
-            <a href="{{ route('pendaftaran.create') }}" class="btn btn-primary">Kembali ke Formulir Pendaftaran</a>
+            <a href="{{ route('siswa.create.pendaftaran') }}" class="btn btn-primary">Kembali ke Formulir Pendaftaran</a>
         </div>
     </div>
 </div>
