0c7ab5bc3109b56eb52b027f204883308fe32dc3
Reinhard Batubara <112596540+ReinhardBatubara@users.noreply.github.com>
2025-09-24 16:20:14 +0700

    Update Fasilitas

--- codeprogram/resources/views/superadmin/fasilitas.blade.php
+++ codeprogram/resources/views/superadmin/fasilitas.blade.php
@@ -137,7 +137,7 @@
                                             </div>
                                             <h3 class="empty-title">Belum ada fasilitas</h3>
                                             <p class="empty-subtitle">Mulai dengan menambahkan fasilitas pertama Anda</p>
-                                            <button class="btn btn-primary" onclick="window.location.href='{{ route('fasilitas.create') }}'">
+                                            <button class="btn btn-primary" onclick="window.location.href='{{ route('superadmin.fasilitas.create') }}'">
                                                 <i class="fas fa-plus"></i>
                                                 Tambah Fasilitas
                                             </button>
